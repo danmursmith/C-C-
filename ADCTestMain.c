14735f320084acfa7c59a2a4ca4d5c824d86c7a4
Daniel Smith <danielmurraysmith525@gmail.com>
2016-06-02 01:11:25 -0500

    Add files via upload
    Lab 2 PDF contains an in-depth description of the contents of our project.
    
    Lab was intended to teach about critical section debugging and using the ADC built into our TM4C123 microcontroller.

---
+++ ADCTestMain.c
@@ -0,0 +1,208 @@
+// ADCTestMain.c
+// Runs on TM4C123
+// This program periodically samples ADC channel 0 and stores the
+// result to a global variable that can be accessed with the JTAG
+// debugger and viewed with the variable watch feature.
+// Daniel Valvano
+// September 5, 2015
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// center of X-ohm potentiometer connected to PE3/AIN0
+// bottom of X-ohm potentiometer connected to ground
+// top of X-ohm potentiometer connected to +3.3V 
+#include <stdint.h>
+#include <stdlib.h>
+#include "PLL.h"
+#include "ST7735.h"
+#include "fixed.h"
+#include "ADCSWTrigger.h"
+#include "../inc/tm4c123gh6pm.h"
+#define PF2             (*((volatile uint32_t *)0x40025010))
+#define PF1             (*((volatile uint32_t *)0x40025008))
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+uint32_t Jitter(void);
+void PMF(void);
+
+volatile uint32_t ADCvalue;
+uint32_t Index = 0;
+uint32_t ADCtimes[1000];
+uint32_t ADCvalues[1000];
+uint32_t ADCfrequency[1000];
+
+// This debug function initializes Timer0A to request interrupts
+// at a 100 Hz frequency.  It is similar to FreqMeasure.c.
+void Timer0A_Init1000HzInt(void){
+  volatile int32_t delay;
+  DisableInterrupts();
+  // **** general initialization ****
+  SYSCTL_RCGCTIMER_R |= 0x01;      // activate timer0
+  delay = SYSCTL_RCGCTIMER_R;      // allow time to finish activating
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = 0;                // configure for 32-bit timer mode
+  // **** timer0A initialization ****
+                                   // configure for periodic mode
+  TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER0_TAILR_R = 79999;         // start value for 100 Hz interrupts
+  TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
+  // **** interrupt initialization ****
+                                   // Timer0A=priority 2
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
+  NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+}
+
+void Timer0A_Init100HzInt(void){
+  volatile uint32_t delay;
+  DisableInterrupts();
+  // **** general initialization ****
+  SYSCTL_RCGCTIMER_R |= 0x01;      // activate timer0
+  delay = SYSCTL_RCGCTIMER_R;      // allow time to finish activating
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = 0;                // configure for 32-bit timer mode
+  // **** timer0A initialization ****
+                                   // configure for periodic mode
+  TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER0_TAILR_R = 799999;         // start value for 100 Hz interrupts
+  TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
+  // **** interrupt initialization ****
+                                   // Timer0A=priority 2
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
+  NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+}
+
+void Timer0A_Handler(void){
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;    // acknowledge timer0A timeout
+  PF2 ^= 0x04;                   // profile
+  PF2 ^= 0x04;                   // profile
+  ADCvalue = ADC0_InSeq3();
+  PF2 ^= 0x04;                   // profile
+
+  if(Index < 1000) {
+    ADCtimes[Index] = TIMER1_TAR_R;
+    ADCvalues[Index] = ADCvalue;
+		Index++;
+  }
+}
+
+void Timer1_Init(void){
+  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+  //PeriodicTask = task;          // user function
+	//delay = SYSCTL_RCGCTIMER_R;
+  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+  TIMER1_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER1_TAILR_R = 0xFFFFFFFF;    // 4) reload value
+  TIMER1_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER1_ICR_R = 0x00000001;    // 6) clear TIMER1A timeout flag
+//  TIMER1_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF00FF)|0x00008000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 37, interrupt number 21
+  //NVIC_EN0_R = 1<<21;           // 9) enable IRQ 21 in NVIC
+  TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
+}
+
+void Timer1A_Handler(void){
+  TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER1A timeout
+}
+
+int main(void){
+
+  PLL_Init(Bus80MHz);                   // 80 MHz
+  SYSCTL_RCGCGPIO_R |= 0x20;            // activate port F
+  ADC0_InitSWTriggerSeq3_Ch9();         // allow time to finish activating
+  Timer0A_Init1000HzInt();               // set up Timer0A for 100 Hz interrupts
+	Timer1_Init();												// set up Timer1 for 12.5ns interval counting
+  GPIO_PORTF_DIR_R |= 0x06;             // make PF2, PF1 out (built-in LED)
+  GPIO_PORTF_AFSEL_R &= ~0x06;          // disable alt funct on PF2, PF1
+  GPIO_PORTF_DEN_R |= 0x06;             // enable digital I/O on PF2, PF1
+                                        // configure PF2 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF00F)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;               // disable analog functionality on PF
+  PF2 = 0;                              // turn off LED
+	  EnableInterrupts();
+	//ADC0_SAC_R &= 0xFFFFFFF8;	//no hardware oversample  
+  while(Index < 1000){
+		GPIO_PORTF_DATA_R ^= 0x02;
+		PF1 = (PF1*12345678)/1234567+0x02;  // this line causes jitter
+	}
+	DisableInterrupts();
+	
+	ST7735_InitR(INITR_GREENTAB);
+	uint32_t calc_jitter = Jitter();
+	//Timer0A_Init100HzInt();               // set up Timer0A for 100 Hz interrupts
+	ST7735_FillScreen(0);
+	PMF();
+	
+	while(1){
+			//GPIO_PORTF_DATA_R ^= 0x02;
+			PF1 ^= 0x02;  // toggles when running in main
+		}
+}
+
+uint32_t Jitter(){
+		for(int i = 0; i < 999; i++) {
+			ADCtimes[i] = ADCtimes[i + 1] - ADCtimes[i];
+		}
+	
+		uint32_t smallest = ADCtimes[0];
+		uint32_t largest = ADCtimes[0];
+		
+		for(uint32_t i = 0; i<1000; i++){
+			if(ADCtimes[i] < smallest)
+				smallest = ADCtimes[i];
+			if(ADCtimes[i] > largest)
+				largest = ADCtimes[i];
+		}
+		return (largest-smallest);
+		
+}
+//Calculates and plots values for 
+void PMF(){
+	uint32_t min = 0; 
+	uint32_t max = 4096;	//Max value
+	uint32_t mostfrequent = 0;
+	for (uint32_t x = 0; x < 4096; x++) {
+		if (ADCfrequency[x] != 0) {
+			max = x;
+			if (min == 0) {
+				min = x;
+			}
+			if (ADCfrequency[x] > mostfrequent) {
+				mostfrequent = ADCfrequency[x];
+			}
+		}
+		
+	}
+	ST7735_FillScreen(0);
+	//ST7735_XYplotInit(0,0,127,159,ST7735_BLUE);
+	
+//	ST7735_SetCursor(,0);
+	for(uint32_t y = min; y < max; y++){
+	//	ST7735_XYplot(0,0,ST7735_TFTWIDTH*(y-min)/(max-min),ADCfrequency[y],ST7735_BLUE);
+	//	ST7735_DrawPixel(ST7735_TFTWIDTH*(y-min)/(max-min),y,ST7735_BLUE);
+		ST7735_DrawFastVLine(ST7735_TFTWIDTH*(y-min)/(max-min),ADCfrequency[y], 150 , ST7735_WHITE);
+	}
+}
---
+++ fixed.c
@@ -0,0 +1,117 @@
+
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include "ST7735.h"
+#include "PLL.h"
+#include <math.h>
+#include "../inc/tm4c123gh6pm.h"
+
+void ST7735_sDecOut3(int32_t n){
+
+	int nfirst=n/1000;																//n_temp will be the bit 
+
+	if((n>9999)||(n<-9999)){										//top limit is 9999, minimum is -9999, print " *.***" each time n out of bounds
+		printf(" *.***");
+	  return;
+		
+	}else if(nfirst==0 && n<0){
+		
+		printf("-%d", nfirst);
+		n*=-1;
+		printf(".%03d", n%1000);
+	}
+	else if(n<0){															//negative values of n to print exactly 6 characters to the screen
+	  printf("%d", nfirst);
+		n*=-1;
+		printf(".%03d", n%1000);
+		
+		
+	}else if (n>=0){											
+		printf(" %d.", nfirst);
+		printf("%03d",n%1000);
+	}
+}
+void ST7735_uBinOut8(uint32_t n){
+	  //double ndiv=n>>8;
+		//double ndiv=n/256;
+	  uint16_t fract=0;							//right side of decimal for output
+		uint16_t num=0;									//left side
+		
+		if(n>=256000){
+				printf("***.**");
+		}else{		
+			  fract=(n%256)*100;				//conversion in this else statement 
+				
+		  	num=n/256;
+
+			  fract/=256;					 
+				printf("%3d.%.2d",num,fract);//truncate decimals for fract to 2 without rounding
+			
+		
+		}
+}
+/**************ST7735_XYplotInit***************
+ Specify the X and Y axes for an x-y scatter plot
+ Draw the title and clear the plot area
+ Inputs:  title  ASCII string to label the plot, null-termination
+          minX   smallest X data value allowed, resolution= 0.001
+          maxX   largest X data value allowed, resolution= 0.001
+          minY   smallest Y data value allowed, resolution= 0.001
+          maxY   largest Y data value allowed, resolution= 0.001
+ Outputs: none
+ assumes minX < maxX, and miny < maxY
+*/
+
+int32_t xSmallBound, xBigBound, ySmallBound, yBigBound;
+
+void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY){
+		
+		xSmallBound=minX;														//store bounds to be used outside initialization function
+		xBigBound=maxX;
+	  ySmallBound=minY;
+	  yBigBound=maxY;
+		ST7735_FillScreen(0);
+		ST7735_SetCursor(0,0);
+		ST7735_OutString(title);
+	  ST7735_PlotClear(minY, maxY);
+		
+	
+}
+void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[]){
+		for(uint32_t k=0;k<num;k+=1){
+				uint32_t i,j;
+				i=127*(bufX[k]-xSmallBound)/(xBigBound-xSmallBound);
+				j=32+(127*(yBigBound-bufY[k]))/(yBigBound-ySmallBound);
+				ST7735_DrawPixel(i, j, ST7735_BLUE);
+		}
+
+}
+
+void ST7735_Line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color){
+	// TODO - validate input
+	uint16_t xcurrent = x1;
+	uint16_t ycurrent = y1;
+	uint16_t snumerator = y2 - ycurrent;
+	uint16_t sdenominator = x2 - xcurrent;
+	uint16_t divisor;																						
+	if (sdenominator < snumerator){
+		divisor = sdenominator;
+	} else {
+		divisor = snumerator;
+	}
+	while (snumerator%divisor!= 0 && sdenominator%divisor != 0 && divisor > 1){
+		divisor--;
+	}
+	snumerator = snumerator / divisor;
+	sdenominator = sdenominator / divisor;
+	while(xcurrent != x2 && ycurrent != y2){
+		ST7735_DrawPixel(xcurrent, ycurrent, color);
+		xcurrent = xcurrent+ sdenominator;
+		ycurrent = ycurrent+ snumerator;
+	} 
+			
+}
+
+	
